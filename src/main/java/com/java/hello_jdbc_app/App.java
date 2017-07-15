d3a76ca95f606a0444d0dfe3c9c042552f10f862
RajeshKSethi <kumarsethi.rajesh@gmail.com>
2017-07-16 01:24:41 +0530

    commit

---
+++ src/main/java/com/java/hello_jdbc_app/App.java
@@ -0,0 +1,13 @@
+package com.java.hello_jdbc_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
